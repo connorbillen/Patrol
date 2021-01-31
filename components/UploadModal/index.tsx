e741ad0195b3d8e2e5cafa9908aa2e37f7974ee3
Connor Billen <connorrbillen@gmail.com>
2021-01-31 08:40:39 -0500

    Upload instructions

--- components/UploadModal/index.tsx
+++ components/UploadModal/index.tsx
@@ -8,7 +8,7 @@ import {
   CardActions,
   Button
 } from "@material-ui/core";
-import { Dispatch } from "react";
+import { Dispatch, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { Layers, Point, State, Upload } from "../../interfaces";
@@ -24,6 +24,8 @@ const ModalPopup = (): JSX.Element => {
   const state: Upload = useSelector((state: State) => state.Upload);
   const layersState: Layers = useSelector((state: State) => state.Layers)
   const dispatcher: Dispatch<any> = useDispatch();
+  const [showHistorical, setShowHistorical] = useState(true);
+  
   const classes = makeStyles({
     root: {
       width: 400,
@@ -43,6 +45,26 @@ const ModalPopup = (): JSX.Element => {
     pos: {
       marginBottom: 12,
     },
+    instructions: {
+      marginTop: 5,
+      marginBottom: 5
+    },
+    code: {
+      background: '#eee',
+      height: 150,
+      overflowY: 'auto'
+    },
+    extraSmall: {
+      paddingTop: 4,
+      paddingBottom: 4,
+      paddingLeft: 5,
+      paddingRight: 5,
+      fontSize: '0.6rem'
+    },
+    selected: {
+      textDecoration: 'none',
+      backgroundColor: 'rgba(0, 0, 0, 0.06)'
+    }
   })();
 
   const enabledLayers = []
@@ -77,6 +99,10 @@ const ModalPopup = (): JSX.Element => {
 
   const validateJson = (_json: any): boolean => {
     return true;
+  };
+
+  const toggleModal = (): void => {
+    dispatcher({ type: actions.TOGGLE_UPLOAD_MODAL });
   };
 
   const processContent = async (content: any): Promise<void> => {
@@ -135,19 +161,58 @@ const ModalPopup = (): JSX.Element => {
         >
           Upload New Data
         </Typography>
-        <Typography>TODO: Upload instructions go here</Typography>
+        <Typography className={classes.instructions}>
+          New layers can be uploaded using a simple JSON file.
+        </Typography>
+        <Typography className={classes.instructions}>
+          Choose which layer type you'd like to upload for an example file format. Working example files are located
+          in the Github repository.
+        </Typography>
+        <div>
+          <Button className={ `${ classes.extraSmall } ${ showHistorical ? classes.selected : null }` } onClick={(): void => { setShowHistorical(true); }}>
+            Static Data
+          </Button>
+          <Button className={ `${ classes.extraSmall } ${ !showHistorical ? classes.selected : null }` } onClick={(): void => { setShowHistorical(false); }}>
+            Time-based Data
+          </Button>
+        </div>
+        { showHistorical &&  
+          <Typography className={classes.code} noWrap> 
+              &#123; <br />
+              &nbsp;&nbsp;"points": [ <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&#123; "lat": 0.0, "lon": 0.0 &#125;, <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;... < br/>
+              &nbsp;&nbsp;], <br />
+              &nbsp;&nbsp;"time_enabled": 0, <br />
+              &nbsp;&nbsp;"title": "Null Island", <br />
+              &nbsp;&nbsp;"lat": 0, <br />
+              &nbsp;&nbsp;"lon": 0 <br />
+              &#125; <br />
+          </Typography>
+        }
+        { !showHistorical &&  
+          <Typography className={classes.code} noWrap> 
+              &#123; <br />
+              &nbsp;&nbsp;"points": [ <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;// timestamp is unixtime <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&#123; "lat": 0.0, "lon": 0.0, "timestamp": 123456789 &#125;, <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;... < br/>
+              &nbsp;&nbsp;], <br />
+              &nbsp;&nbsp;"time_enabled": 1, <br />
+              &nbsp;&nbsp;"title": "Null Island", <br />
+              &nbsp;&nbsp;"lat": 0, <br />
+              &nbsp;&nbsp;"lon": 0 <br />
+              &#125; <br />
+          </Typography>
+        }
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={uploadFile}>
+        <Button size="small" onClick={ uploadFile }>
           Upload File
         </Button>
       </CardActions>
     </Card>
   );
-
-  const toggleModal = (): void => {
-    dispatcher({ type: actions.TOGGLE_UPLOAD_MODAL });
-  };
 
   return (
     <Modal
--- queries/index.ts
+++ queries/index.ts
@@ -1,50 +1,63 @@
 import { DocumentNode, gql } from "@apollo/client";
 
 const ADD_POINT: DocumentNode = gql`
-    mutation AddPoint($layerID: ID!, $lat: Float!, $lon: Float!, $timestamp: Int) {
+  mutation AddPoint(
+    $layerID: ID!
+    $lat: Float!
+    $lon: Float!
+    $timestamp: Int
+  ) {
     addPoint(layerID: $layerID, lat: $lat, lon: $lon, timestamp: $timestamp) {
-        success
+      success
     }
-    }
+  }
 `;
 
 const ADD_LAYER: DocumentNode = gql`
-    mutation AddLayer($title: String!, $time_enabled: Int!, $lat: Float!, $lon: Float!, $time_start: Int, $time_end: Int) {
-    addLayer(title: $title, time_enabled: $time_enabled, lat: $lat, lon: $lon, time_start: $time_start, time_end: $time_end) {
-        success
-        id
+  mutation AddLayer(
+    $title: String!
+    $time_enabled: Int!
+    $lat: Float!
+    $lon: Float!
+    $time_start: Int
+    $time_end: Int
+  ) {
+    addLayer(title: $title
+      time_enabled: $time_enabled
+      lat: $lat
+      lon: $lon
+      time_start: $time_start
+      time_end: $time_end
+    ) {
+      success
+      id
     }
-    }
+  }
 `;
 
 const GET_POINTS: DocumentNode = gql`
-    query GetPoints($layerIDs: [ID]!, $timestart: Int, $timeend: Int) {
-        points(layerIDs: $layerIDs, timestart: $timestart, timeend: $timeend) {
-            id
-            lat
-            lon
-            timestamp
-        }
+  query GetPoints($layerIDs: [ID]!, $timestart: Int, $timeend: Int) {
+    points(layerIDs: $layerIDs, timestart: $timestart, timeend: $timeend) {
+      id
+      lat
+      lon
+      timestamp
     }
-`
+  }
+`;
 
 const GET_LAYERS: DocumentNode = gql`
-    query GetLayers {
-        layers {
-            id
-            title
-            time_enabled
-            lat
-            lon
-            timestart
-            timeend
-        }
+  query GetLayers {
+    layers {
+      id
+      title
+      time_enabled
+      lat
+      lon
+      timestart
+      timeend
     }
-`
+  }
+`;
 
-export {
-    ADD_LAYER,
-    ADD_POINT,
-    GET_LAYERS,
-    GET_POINTS
-}
+export { ADD_LAYER, ADD_POINT, GET_LAYERS, GET_POINTS };
